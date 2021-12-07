0bfd9e1cac63ca859c449dd87c641ec00f630ecc
pjy1551 <pjy1551@douzone.com>
2021-12-07 10:01:11 +0900

    es6 init setting

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express')
+var cost = require('./lecture/constAndLet/constAndLet')
+
 var app = express()
 app.listen(8099);
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-    let test = 'ES6 tutroial'
-    res.send(test)
+    let main = 'ES6 tutroial test'
+    res.send(cost)
 })
