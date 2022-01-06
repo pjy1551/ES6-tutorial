a45628f5c195e51921950a97c6c67256bc0e46b2
pjy1551 <pjy1551@gmail.com>
2022-01-06 18:07:52 +0900

    constAndLet 소스정리

--- lecture/1.constAndLet/constAndLet.js
+++ lecture/1.constAndLet/constAndLet.js
@@ -1,11 +1,8 @@
 // ES
 let name = 'jooyoung';
 console.log(name)
+// cons 수정불가
 const constName = 'jisung';
 console.log(constName)
-// cons 수정불가
-// constName = 'jpark'
-
-
 
 module.exports =  'constAndLet' ;
